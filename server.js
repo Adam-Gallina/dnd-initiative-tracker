fbb35712e6afe58e97476bdb5ad5215c9b76246f
Adam-Gallina <adamg461@gmail.com>
2022-01-24 13:18:22 -0800

    Set up basic website functionality

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express')
+var exphbs = require('express-handlebars')
+
+var app = express()
+var port = process.env.PORT || 2282
+
+app.engine('handlebars', exphbs.engine({defaultLayout : 'main'}))
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', function(req, res, next) {
+    res.status(200).render('home')
+})
+
+app.get('*', function(req, res) {
+    console.log("404 for " + req.url)
+})
+
+app.listen(port, function() {
+    console.log('-- Server is listening on port ' + port + '!')
+})
